006f5f44f000164bdb826617339a144c3a73cab5
Nick Muravev <murvievnick@gmail.com>
2026-07-27 00:30:38 +0300

    feat: доработки проекта

--- js/main.js
+++ js/main.js
@@ -2,16 +2,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const accordions = document.querySelectorAll('[data-accordion]');
 
     accordions.forEach((accordion) => {
-    const trigger = accordion.querySelector('[data-accordion-trigger]');
-    const content = accordion.querySelector('[data-accordion-content]');
+        const trigger = accordion.querySelector('[data-accordion-trigger]');
+        const content = accordion.querySelector('[data-accordion-content]');
 
-    trigger.addEventListener('click', () => {
-        const isOpen = !content.hidden;
+        trigger.addEventListener('click', () => {
+            const isOpen = !content.hidden;
 
-        content.hidden = isOpen;
-        trigger.setAttribute('aria-expanded', String(!isOpen));
+            content.hidden = isOpen;
+            trigger.setAttribute('aria-expanded', String(!isOpen));
+        });
+
+        trigger.setAttribute('aria-expanded', 'false');
     });
 
-    trigger.setAttribute('aria-expanded', 'false');
-    });
+    const regBtn = document.querySelector('.js-reg-btn');
+    const regBlock = document.querySelector('.js-reg-block');
+    const regSection = document.querySelector('.js-reg-section');
+
+    if(regBtn || regBlock || regSection) {
+        
+        regBtn.addEventListener('click', () => {
+            regSection.classList.add('is-visible');
+            regBlock.classList.add('is-hidden');
+        })
+    }
+
+    const phoneMask = document.querySelector('#phone-mask');
+
+    if (phoneMask) {
+        IMask(phoneMask, {
+                mask: '+{7}(000)000-00-00'
+            }
+        )
+    }
 })
